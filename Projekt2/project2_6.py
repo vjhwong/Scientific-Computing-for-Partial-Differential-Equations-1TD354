ead15634bd11970b13838a7e9c050af30fa5c674
vjhwong <victor.ju.hin.wong@gmail.com>
2023-02-28 11:37:15 +0100

    Removed code used for testing stiffness_matrix_assembler

--- Projekt2/project2_6.py
+++ Projekt2/project2_6.py
@@ -13,22 +13,17 @@ def stiffness_matrix_assembler(x):
     A = spsp.dok_matrix((N + 1, N + 1))  # initialize stiffness matrix
     for i in range(N):  # loop over elements
         h = x[i + 1] - x[i]  # element length
-        # A[i, i] += 1 / h  # assemble element stiffness
-        # A[i, i + 1] += -1 / h
-        # A[i + 1, i] += -1 / h
-        # A[i + 1, i + 1] += 1 / h
-        A[i, i] += 1  # assemble element stiffness
-        A[i, i + 1] += -1
-        A[i + 1, i] += -1
-        A[i + 1, i + 1] += 1
+        A[i, i] += 1 / h  # assemble element stiffness
+        A[i, i + 1] += -1 / h
+        A[i + 1, i] += -1 / h
+        A[i + 1, i + 1] += 1 / h
     A[0, 0] = 1e6  # adjust for BC
     A[N, N] = 1e6
     return A.tocsr()
 
 
 def main():
-    x = stiffness_matrix_assembler(np.linspace(0, 1, 6))
-    print(x.toarray())
+    pass
 
 
 if __name__ == "__main__":
