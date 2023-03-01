0f6435d0dd80cb292b66252f6aa4569c443e828c
vjhwong <victor.ju.hin.wong@gmail.com>
2023-03-01 11:04:20 +0100

    Edited the step function to take in the stiffness matrix, mass matrix and the method of solving the linear system. These are the arguments needed for the right-hand side function in project2_6.py to work.

--- Projekt2/rungekutta4.py
+++ Projekt2/rungekutta4.py
@@ -3,7 +3,7 @@ This module solves ODEs of the form dv/dt = f(v).
 """
 
 
-def step(f, v, t, dt):
+def FEM_step(f, A, v, M, method, t, dt):
     """Take one RK4 step. Return updated solution and time.
     f: Right-hand-side function: dv/dt = f(v)
     v: current solution
@@ -12,10 +12,10 @@ def step(f, v, t, dt):
     """
 
     # Compute rates k1-k4
-    k1 = dt * f(v)
-    k2 = dt * f(v + 0.5 * k1)
-    k3 = dt * f(v + 0.5 * k2)
-    k4 = dt * f(v + k3)
+    k1 = dt * f(A, v, M, method)
+    k2 = dt * f(A, v + 0.5 * k1, M, method)
+    k3 = dt * f(A, v + 0.5 * k2, M, method)
+    k4 = dt * f(A, v + k3, M, method)
 
     # Update solution and time
     v = v + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
